d3ce859755ebe50cec5bcf1aeaf9d4186506ccc4
Paulo Machado <pauloalexandre3d@gmail.com>
2019-10-14 23:03:03 +0100

    refactoring: Done

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,20 +2,24 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Voorlegen';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorlegen';
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext>
+      </button>
+    );
   }
 
   render() {
     return (
       <ColorContext.Consumer>
-        {color => (
-          <button className={`ui button ${color}`}>
-            <LanguageContext.Consumer>
-              {value => this.renderSubmit(value)}
-            </LanguageContext.Consumer>
-          </button>
-        )}
+        {color => this.renderButton()}
       </ColorContext.Consumer>
     );
   }
